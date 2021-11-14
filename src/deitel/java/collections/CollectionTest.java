82781078f155f5d07ecea60cb39ad3ff453ddd98
Andre1312 <alcbarros@gmail.com>
2021-11-13 23:30:41 -0300

    Add files Java Collections test

---
+++ src/deitel/java/collections/CollectionTest.java
@@ -0,0 +1,50 @@
+package deitel.java.collections;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionTest {
+
+    public static void main(String[] args) {
+        String[] colors = {"MAGENTA","RED","BLUE","CYAN"};
+        List<String> list = new ArrayList<String>();
+
+        for (String color:colors)
+            list.add(color);
+
+        String[] removeColors = {"RED","WHITE","BLUE"};
+        List<String> removeList = new ArrayList<String>();
+
+        for (String color : removeColors)
+            removeList.add(color);
+
+        System.out.println("ArrayList: ");
+
+        for(int count = 0 ; count < list.size(); count++)
+            System.out.printf("%s ", list.get(count));
+
+        removeColors(list, removeList);
+
+        System.out.printf("%n%nArrayList after calling removeColors");
+
+        for (String color: list)
+            System.out.printf("%s ", color);
+
+    }
+
+    private static void removeColors(Collection<String> collection1, Collection<String> collection2){
+
+        System.out.println("Calling RemoveColora");
+        Iterator<String> iterator = collection1.iterator();
+        int i=0;
+        while(iterator.hasNext()){
+            if (collection2.contains(iterator.hasNext()))
+                iterator.remove();
+            System.out.println("Iteration: "+i);
+            i++;
+        }
+    }
+
+}
